d55567e72480b2044c20f9eaf1e7bbf3d198d24f
jere-mie <jere-mie@users.noreply.github.com>
2021-03-17 02:01:09 -0400

    big changes, now open-ly

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ import json
 from flask_wtf import FlaskForm
 from wtforms.validators import DataRequired, Length, EqualTo, ValidationError
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, FileField
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required, UserMixin
+import re
+import os
+
 # this is for getting the secret key
 with open('secrets.json') as f:
   data = json.load(f)
@@ -14,7 +18,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = data['secret_key']
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
-password = data['password']
 
 # Model for links for the database
 class Link(db.Model):
@@ -22,32 +25,50 @@ class Link(db.Model):
     url = db.Column(db.String(50), unique=True, nullable=False)
     dest = db.Column(db.String(150), nullable=False)
 
+if not os.path.exists('site.db'):
+    db.create_all()
+
+# setting up login stuff
+login_manager = LoginManager(app)
+login_manager.login_view='login'
+login_manager.login_message_category='info'
+
+class User(UserMixin):
+    pass
+
+@login_manager.user_loader
+def user_loader(username):
+  user = User()
+  user.id = username
+  return user
+
+# password for logging in
+password = data['password']
+domain = data['domain']
+
 # Form for adding links to the database
 class AddLink(FlaskForm):
-    password = PasswordField('Password', validators=[DataRequired(), Length(min=2, max=20)])
     url = StringField('Custom URL Ending', validators=[DataRequired(), Length(min=3, max=20)])
     dest = StringField('Destination URL', validators=[DataRequired(), Length(min=3, max=148)])
     submit = SubmitField('Submit')
+
+# Login Form
+class LoginForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=2, max=20)])
+    submit = SubmitField('Submit')
+
 
 # redirect to homepage
 @app.route('/', methods=['GET', 'POST'])
 def home():
     form = AddLink()
-    if form.validate_on_submit():
-        if form.password.data == password:
+    if current_user.is_authenticated:
+        if form.validate_on_submit():
             # getting rid of bad chars
+            form.url.data = form.url.data.strip()
             form.url.data = form.url.data.replace(' ', '-')
-            form.url.data = form.url.data.replace('.', '-')
-            form.url.data = form.url.data.replace(',', '-')
-            form.url.data = form.url.data.replace('?', '-')
-            form.url.data = form.url.data.replace('!', '-')
-            form.url.data = form.url.data.replace('@', '-')
-            form.url.data = form.url.data.replace('#', '-')
-            form.url.data = form.url.data.replace('$', '-')
-            form.url.data = form.url.data.replace('%', '-')
-            form.url.data = form.url.data.replace('^', '-')
-            form.url.data = form.url.data.replace('&', '-')
-            form.url.data = form.url.data.replace('*', '-')
+            form.url.data = re.sub(r'[^a-zA-Z0-9-]', '-', form.url.data)
+
             link = Link.query.filter_by(url=form.url.data).first()
             if link:
                 link.dest = form.dest.data
@@ -56,18 +77,42 @@ def home():
                 link = Link(url=form.url.data, dest=form.dest.data)
                 db.session.add(link)
                 db.session.commit()
-            flash(f'Successfully Created Link: \nhttps://ly.bornais.ca/{form.url.data}', 'success')
+            flash(f'Successfully Created Link: \nhttp://{domain}/{form.url.data}', 'success')
+    return render_template('home.html', form=form, data=data)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.password.data == password:
+            user = User()
+            user.id = "Jeff"
+            login_user(user, remember=True)
+            flash('login worked', 'success')
+            return redirect(url_for('home'))
         else:
-            flash('Error Adding/Updating Link', 'danger')
-    return render_template('home.html', form=form)
+            flash('incorrect password!', 'danger')
+    return render_template('login.html', form=form, data=data)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 
 @app.route('/view', methods=['GET'])
 def view():
-    links = Link.query.all()
-    return render_template('links.html', links=links)
-
+    if current_user.is_authenticated:
+        links = Link.query.all()
+        return render_template('links.html', links=links, data=data)
+    flash('you must be logged in', 'danger')
+    return redirect(url_for('home'))
 @app.route('/delete/<id>', methods=['GET'])
 def delete(id):
+    if not current_user.is_authenticated:
+        return redirect(url_for('home'))
     try:
         link = Link.query.filter_by(url=id).first()
     except:
@@ -76,7 +121,7 @@ def delete(id):
     if link:
         db.session.delete(link)
         db.session.commit()
-        flash(f'successfully deleted link ly.bornais.ca/{link.url}', 'success')
+        flash(f'successfully deleted link {domain}/{link.url}', 'success')
     else:
         flash('error deleting link')
     return redirect(url_for('home'))
